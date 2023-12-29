aa60638ada8e0677214b8e73b15098799a18f726
TimeTravelPenguins <TimeTravelPenguin@gmail.com>
2023-12-30 00:17:28 +1100

    Continued working on progress display

--- src/BAET/progress_status.py
+++ src/BAET/progress_status.py
@@ -3,18 +3,26 @@ from __future__ import annotations
 from enum import StrEnum
 from typing import Literal
 
-import rich.repr
+from rich.console import Console, ConsoleOptions, RenderResult
+from rich.repr import Result
 
 
 type ProgressStatusLiteral = Literal["Waiting", "Running", "Completed", "Error"]
 
 
-@rich.repr.auto
 class ProgressStatus(StrEnum):
     Waiting = "Waiting"
     Running = "Running"
     Completed = "Completed"
     Error = "Error"
+
+    def __rich_repr__(self) -> Result:
+        yield repr(self)
+
+    def __rich_console__(
+        self, console: Console, options: ConsoleOptions
+    ) -> RenderResult:
+        yield repr(self)
 
 
 if __name__ == "__main__":
--- src/BAET/progress_style.py
+++ src/BAET/progress_style.py
@@ -1,10 +1,12 @@
 from collections.abc import Mapping
 
+from rich.console import Console, ConsoleOptions, RenderResult
 from rich.repr import Result
 from rich.style import Style
 from rich.text import Text
 
-from .progress_status import ProgressStatus, ProgressStatusLiteral
+from BAET._theme import app_theme
+from BAET.progress_status import ProgressStatus, ProgressStatusLiteral
 
 
 def parse_style(style: Style | str) -> Style:
@@ -27,10 +29,13 @@ class ProgressStyle:
         current_status: ProgressStatusLiteral | ProgressStatus = ProgressStatus.Waiting,
     ):
         default_style_dict: dict[ProgressStatus, Style] = {
-            ProgressStatus.Waiting: parse_style(waiting_style or "white"),
-            ProgressStatus.Running: parse_style(running_style or "blue"),
-            ProgressStatus.Waiting.Completed: parse_style(completed_style or "green"),
-            ProgressStatus.Waiting.Error: parse_style(error_style or "red"),
+            key: parse_style(value or default)
+            for key, value, default in [
+                (ProgressStatus.Waiting, waiting_style, "white"),
+                (ProgressStatus.Running, running_style, "blue"),
+                (ProgressStatus.Completed, completed_style, "green"),
+                (ProgressStatus.Error, error_style, "red"),
+            ]
         }
 
         if style_dict is not None:
@@ -41,7 +46,17 @@ class ProgressStyle:
             default_style_dict.update(parsed_styles)
 
         self.style_dict: Mapping[ProgressStatus, Style] = default_style_dict
-        self._current_status: ProgressStatus = ProgressStatus(current_status)
+        self.current_status = current_status
+
+    def __call__(self, text: str, *args, **kwargs) -> Text:
+        text = Text(
+            text=text,
+            style=self.style_dict[self.current_status],
+            *args,
+            **kwargs,
+        )
+
+        return text
 
     @property
     def current_status(self) -> ProgressStatus:
@@ -51,30 +66,81 @@ class ProgressStyle:
     def current_status(self, status: ProgressStatusLiteral | ProgressStatus):
         self._current_status = ProgressStatus(status)
 
-    def __call__(self, text: str, *args, **kwargs) -> Text:
-        text = Text(
-            text=text,
-            style=self.style_dict[self._current_status],
-            *args,
-            **kwargs,
-        )
-
-        return text
-
     def __rich_repr__(self) -> Result:
-        yield "_current_status", self._current_status
+        yield f"current_status: {self.current_status}"
         yield "style_dict", self.style_dict
+
+    def __rich_console__(
+        self, console: Console, options: ConsoleOptions
+    ) -> RenderResult:
+        out = {"current_status": self.current_status, **self.style_dict}
+        with console.capture() as capture:
+            console.print(out)
+        yield Text.from_ansi(capture.get())
 
 
 if __name__ == "__main__":
-    import rich
+    from rich.console import Group, group, Console
+    from rich.pretty import pprint
+    from rich.text import Text
+    from rich.panel import Panel
+    from rich.padding import Padding
 
-    style = ProgressStyle(
+    console = Console(theme=app_theme)
+
+    basic_style = ProgressStyle(
         {"Completed": "bold green", ProgressStatus.Error: "bold red"},
         running_style="italic bold cyan",
         waiting_style="underline yellow",
     )
 
-    for status in ["Waiting", "Completed", "Running", "Error"]:
-        style.current_status = status
-        rich.print(style(f'This is the style for "{status}"'))
+    theme_style = ProgressStyle(
+        {
+            ProgressStatus.Waiting: app_theme.styles["status.waiting"],
+            ProgressStatus.Running: app_theme.styles["status.running"],
+            ProgressStatus.Completed: app_theme.styles["status.completed"],
+            ProgressStatus.Error: app_theme.styles["status.error"],
+        },
+        current_status="Running",
+    )
+
+    @group()
+    def apply_styles(style: ProgressStyle, message: str):
+        for status in ["Waiting", "Completed", "Running", "Error"]:
+            style.current_status = status
+            yield style(message.format(status))
+
+    @group()
+    def make_style_group(style: ProgressStyle, message: str):
+        with console.capture() as pretty_capture:
+            pprint(
+                {key.value: val.color.name for key, val in style.style_dict.items()},
+                console=console,
+                expand_all=True,
+                indent_guides=False,
+            )
+        with console.capture() as repr_capture:
+            console.print(
+                style,
+                "\n[u]Demo Application:[/]",
+                Padding(apply_styles(style, message), pad=(0, 0, 0, 3)),
+            )
+
+        yield Text.from_ansi(f"Input Style: {pretty_capture.get()}\n")
+        yield Text.from_ansi(f"Resulting ProgressStyle: {repr_capture.get()}")
+
+    layout = Group(
+        Panel(
+            make_style_group(basic_style, "This is the style for ProgressStyle.{0}"),
+            title="[bold green]Basic Style",
+        ),
+        Panel(
+            make_style_group(
+                theme_style,
+                'This is the style for "ProgressStyle.{0}", taken from app_theme in BAET',
+            ),
+            title="[bold green]Theme Style",
+        ),
+    )
+
+    console.print(layout)
