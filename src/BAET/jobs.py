bfc5e8e6c71e0ffee8f362939b8e54d6298a035e
TimeTravelPenguin <TimeTravelPenguin@gmail.com>
2024-01-04 22:11:21 +1100

    Corrected types

--- src/BAET/jobs.py
+++ src/BAET/jobs.py
@@ -32,7 +32,7 @@ class FFmpegJob:
         return 1_000_000 * float(stream["duration_ts"]) * float(Fraction(stream["time_base"]))
 
     def stream(self, index: StreamIndex) -> AudioStream:
-        stream: AudioStream = first_true(
+        stream: AudioStream | None = first_true(
             self.audio_streams,
             default=None,
             pred=lambda st: st["index"] == index,
