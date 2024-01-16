c127c6cb76d07dd79cb3278bf37fd05a1bf8f8d9
TimeTravelPenguin <35803538+TimeTravelPenguin@users.noreply.github.com>
2024-01-16 11:13:55 +1100

    Moved bundle script to scripts dir

--- bundles/new_bundle.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
-
-time=$(date +%Y-%m-%d_%H-%M-%S)
-fname=$SCRIPT_DIR/$time.bundle
-
-echo Creating bundle: $fname
-git bundle create $fname --all
