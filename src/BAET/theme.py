73215d4585fdb490fcf80ca5eaacf36c2a6f1b20
TimeTravelPenguin <35803538+TimeTravelPenguin@users.noreply.github.com>
2024-03-04 11:33:13 +1100

    General cleanup and removal of old code

--- src/BAET/theme.py
+++ src/BAET/theme.py
@@ -1,15 +1,11 @@
+"""Theme options used by Rich library for the app."""
+
 from typing import Final
 
 from rich.theme import Theme
 
 app_theme_dict: Final[dict[str, str]] = {
     "app.version": "italic bright_cyan",
-    # TODO: Remove
-    "argparse.arg_default": "dim italic",
-    "argparse.arg_default_parens": "dim",
-    "argparse.arg_default_value": "not italic bold dim",
-    "argparse.help_keyword": "bold blue",
-    "argparse.debug_todo": "reverse bold indian_red",
     # Help screen
     "keyword": "bold blue",
     "todo": "reverse bold indian_red",
